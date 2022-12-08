4945a30a3bdec91542ec78360ee69bd4a0bf8a88
Vory7 <72459182+Vory7@users.noreply.github.com>
2022-12-08 15:11:03 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,21 @@
 #define inf 100000
 using namespace std;
 struct Edges {
-int u, v, w;
+    int u, v, w;
 };
 int e;
 
 //алгоритм Беллмана-Форда
 void bellman_ford(Edges edge[], int n, int start, int end) {
     int i, j, d[n];
-    int **s = new int*[n];
-    for (int i = 0; i < n; i++) {
+    int **s = new int*[2];
+    for (int i = 0; i < 2; i++) {
         s[i] = new int[n];
-        s[i][start] = 1;
-        s[i][i] = 2;
+    }
+    
+    for (int j = 0; j < n; j++) {
+        s[0][j] = j;
+        s[1][j] = start;
     }
     
     for (i = 0; i < n; i++) {
@@ -22,36 +25,53 @@ void bellman_ford(Edges edge[], int n, int start, int end) {
     }
     d[start] = 0;
     
-    /*for (int i = 0; i < e; i++) {
-    cout « edge[i].v + 1 « "->" « edge[i].u + 1 « " = " « edge[i].w « endl;
-    }*/
-    
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < e; j++) {
             if (d[edge[j].v] + edge[j].w < d[edge[j].u]) {
                 d[edge[j].u] = d[edge[j].v] + edge[j].w;
-                s[edge[j].u][edge[j].v] = s[edge[j].u][edge[j].u];
-                s[edge[j].u][edge[j].u]++;
+                s[1][edge[j].u] = edge[j].v;
             }
         }
     }
     
-    if (d[end] == inf) {
-        cout << endl << start + 1 << "->" << i + 1 << " = " << "Not";
-    } else {
-        int c = 1;
-        for (int i = 0; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
-                if (j == c) cout << s[end][j] << "->";
-                c++;
+    // if (d[i] == inf) {
+    //     cout << endl << start + 1 << "->" << end + 1 << " = " << "Not";
+    // } else {
+    //     int ptr = s[1][end], sch[n] = {-1}, j = 0;
+    //     while (ptr != start) {
+    //         sch[j] = ptr;
+    //         ptr = s[1][ptr];
+    //         j++;
+    //     }
+    //     cout << endl << start + 1 << "->";
+    //     for (int k = j - 1; k >= 0; k--) {
+    //         cout << sch[k] + 1 << "->";
+    //     }
+    //     cout << end + 1 << " = " << d[end];
+    // }
+    
+    for (int i = 0; i < n; i++) {
+        if (d[i] == inf) {
+            cout << endl << start + 1 << "->" << i + 1 << " = " << "Not";
+        } else {
+            int ptr = s[1][i], sch[n] = {-1}, j = 0;
+            while (ptr != start) {
+                sch[j] = ptr;
+                ptr = s[1][ptr];
+                j++;
             }
+            cout << endl << start + 1 << "->";
+            for (int k = j - 1; k >= 0; k--) {
+                cout << sch[k] + 1 << "->";
+            }
+            cout << i + 1 << " = " << d[i];
         }
     }
 }
 //главная функция
 int main() {
     setlocale(LC_ALL, "Rus");
-    int n, w, start, end;
+    int n, w, start, end = 0;
     e = 0;
     cout << "Количество вершин > ";
     cin >> n;
@@ -71,7 +91,7 @@ int main() {
                 }
                 if(check) {
                     cout << "Вес " << i + 1 << "->" << j + 1 << " > ";
-                    w = rand() % 10 + 1; cout << w << endl;
+                    w = rand() % 10 - 11; cout << w << endl;
                     //cin >> w;
                     if (w != 0) {
                         edge[e].w = w;
@@ -86,7 +106,7 @@ int main() {
     }
     cout << "Стартовая вершина > ";
     cin >> start;
-    cout << "Конечная вершина > ";
-    cin >> end;
-    bellman_ford(edge, n, start - 1, end - 1);
+    // cout << "Конечная вершина > ";
+    // cin >> end;
+    bellman_ford(edge, n, start - 1, end);
 }
